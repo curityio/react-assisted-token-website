06606a7302225c45ddcf8e788aa67ad354ede7ec
Atif Saddique <atif.saddique4@gmail.com>
2018-02-10 20:09:16 +0500

    Fetch curity server configuration

--- src/App.js
+++ src/App.js
@@ -1,21 +1,40 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {ISSUER} from "./constants";
+import axios from "axios";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    config = {};
+
+    constructor() {
+        super();
+        this.loadConfiguration();
+    }
+
+    render() {
+
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <h1 className="App-title">Welcome to React</h1>
+                </header>
+                <p className="App-intro">
+                    To get started, edit <code>src/App.js</code> and save to reload.
+                </p>
+            </div>
+        );
+    }
+
+
+    loadConfiguration() {
+        axios.get(ISSUER + "/.well-known/openid-configuration")
+            .then(response => {
+                this.config = response;
+                console.log(response);
+            });
+    }
 }
 
 export default App;
---
+++ src/constants.js
@@ -0,0 +1,4 @@
+export const ISSUER = "https://localhost:8443/~";
+export const CLIENT_ID = "client-assisted-example";
+export const API_URL = "http://127.0.0.1:8100";
+export const AUTH_SERVER_ORIGIN = "http://127.0.0.1:8100";
