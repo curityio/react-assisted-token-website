ef1cdff51cab3e77f215315022c901a663049eb7
Atif Saddique <atif.saddique4@gmail.com>
2018-02-12 15:49:47 +0500

    remove unnecessary code and files

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
-import './App.css';
 import {API_URL, AUTH_SERVER_ORIGIN, CLIENT_ID, ISSUER} from "./constants";
 import axios from "axios";
 
@@ -42,10 +40,6 @@ class App extends Component {
 
         return (
             <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo"/>
-                    <h1 className="App-title">Welcome to React</h1>
-                </header>
                 <p className="App-intro">
                     {button}
                     <br/>
